6ca04b7cb2da77259602bf6cee21c226df19d258
Arthur Baker <artbaker82@gmail.com>
2021-08-06 13:52:43 -0700

    init scaffold started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, Fragment } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import DashBoard from "./Components/DashBoard/DashBoard";
+import Header from "./Components/Header/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" render={() => <DashBoard />} />
+        <Redirect to="/" />
+      </Switch>
+    </React.Fragment>
   );
 }
 
