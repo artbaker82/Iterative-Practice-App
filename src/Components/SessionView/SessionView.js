c67b57b96a5166ebddee6ef03ecd46ee026212af
Arthur Baker <artbaker82@gmail.com>
2021-08-13 16:09:27 -0700

    practice session screen started

---
+++ src/Components/SessionView/SessionView.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useLocation } from "react-router";
+
+const SessionView = () => {
+  const location = useLocation();
+  const { list } = location.state;
+
+  return (
+    <div>
+      {" "}
+      In practice List:{" "}
+      {list.items.map((item) => {
+        return (
+          <p>
+            Title: {item.title}, Timer set: {item.timer}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SessionView;
